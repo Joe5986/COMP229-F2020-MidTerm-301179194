52c3edd6ae0ad01e3edf8c8346a8f00e63d9e1ed
Joe5986 <yzhou193@my.centennialcollege.ca>
2022-10-30 08:38:17 -0400

    mongoDB atlas link

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,7 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  //"URI": "mongodb://localhost/books229"
+
+  //cloud MongoDB
+  "URI": "mongodb+srv://Joe5986:i3YvOeKKBbXCCozp@cluster0.es1otjy.mongodb.net/?retryWrites=true&w=majority"
 };
